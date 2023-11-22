f067627aeb896e1619c09afa4fda0e025cffc986
Amina Rajab <itsaminarajab@gmail.com>
2023-11-22 14:38:33 +0300

    test

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -3,67 +3,67 @@ import './Nav.css'
 export function Navbar(){
     return(
         <div>
-            {/* <nav class="navbar navbar-expand-lg sticky-top bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" style={{paddingLeft:"150px"}}>Amina Rajab</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            {/* <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" style={{paddingLeft:"150px"}}>Amina Rajab</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse mb-2" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div className="collapse navbar-collapse mb-2" id="navbarTogglerDemo02">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 </ul>
-                <form class="d-flex" role="search">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <form className="d-flex" role="search">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <li className="nav-item">
+                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
                 </form>
                 </div>
             </div>
             </nav> */}
-            <nav class="navbar navbar-dark " style={{marginBottom:"5px"}}>
-                <div class="container-fluid">
-                <a class="navbar-brand" style={{paddingLeft:"150px",paddingTop:"40px"}}>Amina Rajab</a>
-                    <a class="navbar-toggler"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <nav className="navbar navbar-dark " style={{marginBottom:"5px"}}>
+                <div className="container-fluid">
+                <a className="navbar-brand" style={{paddingLeft:"150px",paddingTop:"40px"}}>Amina Rajab</a>
+                    <a className="navbar-toggler"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                     <span >Menu</span>
                     </a>
-                    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Projects</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
-                        {/* <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {/* <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Services Offered
                             </a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <ul className="dropdown-menu dropdown-menu-dark">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
                             <li>
-                                <hr class="dropdown-divider"/>
+                                <hr className="dropdown-divider"/>
                             </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li> */}
                         </ul>
